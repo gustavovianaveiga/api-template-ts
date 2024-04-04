11040af38219dfd5623fa0b7839ef0ec18c27d63
veiga <gustavovianaveiga721@gmail.com>
2024-04-04 10:42:39 -0300

    Update JWTverify.ts

--- src/middleware/JWTverify.ts
+++ src/middleware/JWTverify.ts
@@ -3,11 +3,10 @@ import jwt from 'jsonwebtoken'
 
 function JWTverify(req: Request, res: Response, next: NextFunction) {
 
-    const token: string | string[] = req.headers['x-access-token']!
-    const tokenString: string = Array.isArray(token) ? token[0] : token
-    const hash: string = process.env.HASH!
+    const token:string = req.headers['x-access-token'] as string
+    const hash: string = process.env.HASH as string
 
-    jwt.verify(tokenString, hash, (err, decoded) => {
+    jwt.verify(token, hash, (err, decoded) => {
         if (err) return res.status(401).json({ status: "nao autenticado" })
 
 
